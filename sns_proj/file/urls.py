779a034a67edc0db8f4a4d0e55b7e10b50d1c9eb
yc <yashchaubey2001@gmail.com>
2019-11-22 02:34:29 +0530

    sharpened

--- sns_proj/file/urls.py
+++ sns_proj/file/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import url
-
-from .views import HomePgView, EnterImgView ,PreView
+from .views import HomePgView, EnterImgView , PreView, SharpView
 
 urlpatterns = [
     url(r'^$', HomePgView.as_view(), name = 'base'),
     url(r'^enter/$',EnterImgView.as_view(), name = 'Enter_image'),
     url(r'^edit1/$',PreView.as_view(),name='edit1'),
+    url(r'^edit1/sharp/$',SharpView.as_view(),name='sharpen')
 ]
--- sns_proj/file/views.py
+++ sns_proj/file/views.py
@@ -1,20 +1,32 @@
-from django.views.generic import TemplateView,ListView, CreateView
+from django.views.generic import TemplateView,ListView,CreateView, View
 from .models import FILE
+from django.shortcuts import render
 from .forms import EnterImg
 from django.urls import reverse_lazy
+import os,sys
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 class HomePgView(TemplateView):
     # model = FILE
     template_name = 'base.html'
 
-class EnterImgView(CreateView,ListView):
+class EnterImgView(CreateView):
     model = FILE
     form_class = EnterImg
     template_name = 'enter.html'
     success_url = reverse_lazy('edit1')
 
-class PreView(ListView):
-    model = FILE
-    template_name = 'edit1.html'
-    # def get_context_object_name(self,object_list):
-    #     return self.context_object_name
+class PreView(View):
+    def get(self,request):
+        obj = FILE.objects.order_by('-pk')[0]
+        return render(request, 'edit1.html', {'obj' : obj})
+
+class SharpView(View):
+    def get(self, request):
+        obj = FILE.objects.order_by('-pk')[0]
+        loc = obj.data.url
+        outp = BASE_DIR+'/media/images/output.jpg'
+        loc1 = BASE_DIR+ "/static/source "+BASE_DIR+loc+' '+ BASE_DIR+'/media/images/output.jpg ' +'3'
+        os.system(loc1)
+        return render(request, 'sharp.html',{'path':outp})

061b18ac8a3ee5fc69c7187dcb697e0589470d7f
yc <yashchaubey2001@gmail.com>
2019-11-25 03:37:20 +0530

    frontended

--- sns_proj/file/urls.py
+++ sns_proj/file/urls.py
@@ -3,7 +3,7 @@ from .views import HomePgView, EnterImgView , PreView, SharpView, BoxBlurView, E
 
 urlpatterns = [
     url(r'^$', HomePgView.as_view(), name = 'base'),
-    url(r'^enter/$',EnterImgView.as_view(), name = 'Enter_image'),
+    url(r'^enter/$',EnterImgView.as_view(), name = 'enter'),
     url(r'^edit1/$',PreView.as_view(),name='edit1'),
     url(r'^edit1/sharp/$',SharpView.as_view(),name='sharpen'),
     url(r'^edit1/boxblur/$',BoxBlurView.as_view(),name='boxblur'),

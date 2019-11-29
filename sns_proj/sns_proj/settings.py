001ef1618a2fd3607625bd312bd92bb6851916a2
yc <yashchaubey2001@gmail.com>
2019-11-30 00:06:58 +0530

    post-proj

--- sns_proj/sns_proj/settings.py
+++ sns_proj/sns_proj/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'file.apps.FileConfig',   
-    'django_cleanup.apps.CleanupConfig',
+    # 'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
